ac24427b00136ea3a3fdb9255814334f4172896b
sovonbd <sovonsdp@gmail.com>
2024-01-25 23:35:18 -0600

    updated the design in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import Navbar from "@/scenes/navbar";
 import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
+
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<string>("home");
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
   return (
     <>
-      <div className=" bg-gray-20">app</div>
       <Navbar
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}></Navbar>
